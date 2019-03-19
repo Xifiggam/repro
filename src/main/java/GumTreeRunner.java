ae1700c338bc44e882544714b5b0fbd6e69d9a46
phmoll <philipp.moll@aau.at>
2019-03-19 11:38:02 +0100

    BUGFIX read java files from file

--- src/main/java/GumTreeRunner.java
+++ src/main/java/GumTreeRunner.java
@@ -4,6 +4,7 @@ import com.github.gumtreediff.gen.jdt.JdtTreeGenerator;
 import com.github.gumtreediff.matchers.Matcher;
 import com.github.gumtreediff.matchers.Matchers;
 import com.github.gumtreediff.tree.ITree;
+import convenience.RTED;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,12 +12,12 @@ import java.util.List;
 public class GumTreeRunner {
 
     public static void main(String[] args) throws IOException {
-        String file1 = "file_v0.java";
-        String file2 = "file_v1.java";
+        String file1 = "src/main/java/GumTreeRunner.java";
+        String file2 = file1;
 
         //JDT Tree
-        ITree srcTree = new JdtTreeGenerator().generateFromString(file1).getRoot();
-        ITree dstTree = new JdtTreeGenerator().generateFromString(file2).getRoot();
+        ITree srcTree = new JdtTreeGenerator().generateFromFile(file1).getRoot();
+        ITree dstTree = new JdtTreeGenerator().generateFromFile(file2).getRoot();
 
         runGumTree(srcTree, dstTree);
     }
